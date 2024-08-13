183c7be3b2ef8feea6b2da9d687590c86d3afd36
Matheus Vanzan <vanzan2015@gmail.com>
2024-08-13 13:32:47 +0000

    add Array example

--- JAVA/Exemplos/src/Array.java
+++ JAVA/Exemplos/src/Array.java
@@ -1,3 +1,32 @@
+import java.util.Arrays;
+
 public class Array {
+
+    public static void main(String[] args) {
+        
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        System.out.println(cars[0]); // Volvo        
+        System.out.println(cars.length); // 4
+
+        System.out.println(cars); // [Ljava.lang.String;@6504e3b2
+        System.out.println(Arrays.toString(cars)); // [Volvo, BMW, Ford, Mazda]
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+        String[] cars2;
+
+
+
+        System.out.println(Arrays.toString(cars));
+        
+    }
+
 
 }
