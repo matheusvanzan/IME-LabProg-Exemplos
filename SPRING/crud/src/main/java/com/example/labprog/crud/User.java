fe0c82b0beb6ce3ae6f637d2af77a674d0c6ce7f
Matheus Vanzan <vanzan2015@gmail.com>
2024-08-23 10:08:22 -0300

    add mock users

--- SPRING/crud/src/main/java/com/example/labprog/crud/User.java
+++ SPRING/crud/src/main/java/com/example/labprog/crud/User.java
@@ -1,5 +1,7 @@
 package com.example.labprog.crud;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,36 +9,65 @@ import jakarta.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
-    
-  @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
-  private Integer id;
 
-  private String name;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
 
-  private String email;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;  
+    private LocalDateTime lastLogin;
+    private boolean isAdmin;
+    private boolean isActive;
 
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+    public void setAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+    public boolean isActive() {
+        return isActive;
+    }
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }  
 }
--- SPRING/crud/src/main/java/com/example/labprog/crud/UserRepository.java
+++ SPRING/crud/src/main/java/com/example/labprog/crud/UserRepository.java
@@ -2,9 +2,14 @@ package com.example.labprog.crud;
 
 import org.springframework.data.repository.CrudRepository;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
+// A classe será implementada automaticamente pelo Spring
+// Precisamos apenas criar a interface
+
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    // findAll() e save() não precisam ser declarados
+
+    Iterable<User> findByFirstName(String name);
 
 }
