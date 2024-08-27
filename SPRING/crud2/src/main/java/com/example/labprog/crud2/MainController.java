c74848e7ef5eaa51acae61c852e6d4c8ee1719e0
Matheus Vanzan <vanzan2015@gmail.com>
2024-08-27 11:24:06 -0300

    add crud 2

--- SPRING/crud2/src/main/java/com/example/labprog/crud2/MainController.java
+++ SPRING/crud2/src/main/java/com/example/labprog/crud2/MainController.java
@@ -2,13 +2,8 @@ package com.example.labprog.crud2;
 
 import java.util.HashMap;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
